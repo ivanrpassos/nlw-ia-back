2f33d47b34a8559794d7caca89be359229654924
Ivan Passos <ivanpassosfilho@gmail.com>
2023-09-13 07:09:22 -0300

    Aula 2 - Avançando o back-end da aplicação

---
+++ src/routes/get-all-prompts.ts
@@ -0,0 +1,12 @@
+import { FastifyInstance } from 'fastify';
+import { prisma } from '../lib/prisma';
+
+export const getAllPromptsRoute = async (
+  app: FastifyInstance,
+) => {
+  app.get('/prompts', async () => {
+    const prompts = await prisma.prompt.findMany();
+
+    return prompts;
+  });
+};
